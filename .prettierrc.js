5707b466abf6c996bd05ef944fb9b75fba55d603
Farid Mansimli <f.mensimli@asan.gov.az>
2023-03-30 15:49:51 +0400

    nodejs-solidity (1.0.0) ## cümə axşamı, 30 mart 2023 15:49:51 +04 ##

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: false,
+    trailingComma: "all",
+    arrowParens: "avoid",
+    requirePragma: false,
+    printWidth:95
+  };
